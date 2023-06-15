9763abe233f33a6be8933f6b993486ceba5bbece
Frans Gustavson Påsse <frans.gop@hotmail.com>
2023-06-16 01:36:04 +0200

    feat: added mapping of pages and ability to push to the list of so called todos per project

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card bg-base-300">
+    <div class="card-body">
+      <h2 class="card-title text-xl">
+        {{ name }}
+      </h2>
+      <span
+        class="badge-primary rounded-full py-1 px-2 text-center max-w-fit ml-auto"
+        >{{ state }}</span
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: String,
+  state: String,
+});
+</script>
+
+<style scoped></style>
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,11 @@
-const url = "https://release.aws.thmi.cloud/api/v1/projects/list";
+import { Page, Project } from "../models";
+
+const baseURL = "https://release.aws.thmi.cloud/api/v1/";
 const token = import.meta.env.VITE_API_TOKEN;
 
-async function getProjects(): Promise<[]> {
+async function getProjects(): Promise<Project[] | []> {
+  const url = baseURL + "projects/list";
+
   try {
     const response = await fetch(url, {
       method: "GET",
@@ -23,4 +27,28 @@ async function getProjects(): Promise<[]> {
   }
 }
 
-export { getProjects };
+async function getPages(id: string): Promise<Page | undefined> {
+  const url = baseURL + `${id}/page/tree`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to retrieve projects");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { getProjects, getPages };
